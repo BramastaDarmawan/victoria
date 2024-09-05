bb2685bd83af504ef2ac8dd353d1a4802bfc391a
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-05 10:55:14 +0700

    Jawaban

--- pre-test/day-1/Helllo World/src/Main.java
+++ pre-test/day-1/Helllo World/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.Math.*;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
@@ -21,14 +22,26 @@ public class Main {
 //        System.out.println(huruf_Depan);
 //        System.out.println(alasan);
 
-        final double phi = 3.14;
-        double jari, luas;
-        jari = 10;
-        luas = phi * jari * jari;
-        System.out.println(luas);
+//        final double phi = 3.14;
+//        double jari, luas;
+//        jari = 10;
+//        luas = phi * jari * jari;
+//        System.out.println(luas);
+//
+//        jari = 15;
+//        luas = phi * jari * jari;
+//        System.out.println(luas);
+//        double jari;
+//        jari = 10;
+//        System.out.println(Math.PI * Math.pow(10,2));
 
-        jari = 15;
-        luas = phi * jari * jari;
-        System.out.println(luas);
+        for (int i = 1; i <= 20; i++){
+            if (i%2 == 0){
+                System.out.println(i + " adalah genap");
+            }
+            else{
+                System.out.println(i + " adalah ganjil");
+            }
+        }
     }
 }
