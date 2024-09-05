1461affe2db0254b589f6618902e3af79e45752d
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-05 11:04:24 +0700

    Jawaban Latihan

--- pre-test/day-1/Helllo World/src/Main.java
+++ pre-test/day-1/Helllo World/src/Main.java
@@ -35,12 +35,32 @@ public class Main {
 //        jari = 10;
 //        System.out.println(Math.PI * Math.pow(10,2));
 
-        for (int i = 1; i <= 20; i++){
-            if (i%2 == 0){
-                System.out.println(i + " adalah genap");
+//        for (int i = 1; i <= 20; i++){
+//            if (i%2 == 0){
+//                System.out.println(i + " adalah genap");
+//            }
+//            else{
+//                System.out.println(i + " adalah ganjil");
+//            }
+//        }
+
+        int[] number = {10, 75, 76, 47};
+        for (int i = 0; i <= 3; i++){
+            if(number[i]%2 == 0){
+                if(number[i] > 50){
+                    System.out.println(number[i] + " adalah angka genap yang lebih besar dari 50");
+                }
+                else{
+                    System.out.println(number[i] + " adalah angka genap yang lebih kecil dari 50");
+                }
             }
             else{
-                System.out.println(i + " adalah ganjil");
+                if(number[i] > 50){
+                    System.out.println(number[i] + " adalah angka ganjil yang lebih besar dari 50");
+                }
+                else{
+                    System.out.println(number[i] + " adalah angka ganjil yang lebih kecil dari 50");
+                }
             }
         }
     }
