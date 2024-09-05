aff804349af22de32f01fcaea7501f8e6c61f5e5
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-05 10:30:45 +0700

    Jawaban Bramasta

--- pre-test/day-1/Helllo World/src/Main.java
+++ pre-test/day-1/Helllo World/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        int umur;
+        long panjang_kaki;
+        double jarak;
+        float tinggi;
+        char huruf_Depan;
+        boolean alasan;
+        umur = 23;
+        panjang_kaki = 23;
+        tinggi = 175.5f;
+        jarak = 10.5;
+        huruf_Depan = 'B';
+        alasan = true;
+        System.out.println(umur);
+        System.out.println(panjang_kaki);
+        System.out.println(tinggi);
+        System.out.println(jarak);
+        System.out.println(huruf_Depan);
+        System.out.println(alasan);
     }
 }
