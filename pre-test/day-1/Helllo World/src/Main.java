9ed4eb0a6dbc8ceac5efc5a1810e6f62af351435
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-05 10:40:12 +0700

    Jawaban Bramasta

--- pre-test/day-1/Helllo World/src/Main.java
+++ pre-test/day-1/Helllo World/src/Main.java
@@ -1,23 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int umur;
-        long panjang_kaki;
-        double jarak;
-        float tinggi;
-        char huruf_Depan;
-        boolean alasan;
-        umur = 23;
-        panjang_kaki = 23;
-        tinggi = 175.5f;
-        jarak = 10.5;
-        huruf_Depan = 'B';
-        alasan = true;
-        System.out.println(umur);
-        System.out.println(panjang_kaki);
-        System.out.println(tinggi);
-        System.out.println(jarak);
-        System.out.println(huruf_Depan);
-        System.out.println(alasan);
+        //System.out.println("Hello World!");
+
+//        int umur;
+//        long panjang_kaki;
+//        double jarak;
+//        float tinggi;
+//        char huruf_Depan;
+//        boolean alasan;
+//        umur = 23;
+//        panjang_kaki = 23;
+//        tinggi = 175.5f;
+//        jarak = 10.5;
+//        huruf_Depan = 'B';
+//        alasan = true;
+//        System.out.println(umur);
+//        System.out.println(panjang_kaki);
+//        System.out.println(tinggi);
+//        System.out.println(jarak);
+//        System.out.println(huruf_Depan);
+//        System.out.println(alasan);
+
+        final double phi = 3.14;
+        double jari, luas;
+        jari = 10;
+        luas = phi * jari * jari;
+        System.out.println(luas);
+
+        jari = 15;
+        luas = phi * jari * jari;
+        System.out.println(luas);
     }
 }
