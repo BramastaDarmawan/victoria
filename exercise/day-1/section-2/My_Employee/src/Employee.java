afd464cb0e07998b8a608385f81662f891775e07
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-05 14:20:16 +0700

    Latihan OOP

---
+++ exercise/day-1/section-2/My_Employee/src/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    String nama, npk, address;
+    long salary, age;
+
+    void sayHello(){
+        System.out.println("Hello World!");
+    }
+}
---
+++ exercise/day-1/section-2/My_Employee/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee karyawan = new Employee();
+        karyawan.nama = "I Dewa Gede Ngurah Bramasta Darmawan";
+        karyawan.npk = "68767";
+        karyawan.age = 23;
+        karyawan.address = "Jalan Raya Lebak Bulus";
+        karyawan.salary = 7150000;
+        karyawan.sayHello();
+    }
+}
