b0022017eb6e137d97b415e1fe951209a9b15ef4
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-06 13:21:46 +0700

    Spring Boot

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.Builder;
 
@@ -36,15 +37,21 @@ public class FirstApplication {
 //		return new ResponseEntity<>(id, HttpStatus.OK);
 //	}
 
-	@GetMapping("/users")
-	public ArrayList<GetAssetResponse> asset(){
-		ArrayList<GetAssetResponse> list = new ArrayList<>();
-		GetAssetResponse response1 = GetAssetResponse.builder().id(2323L).fullname("Bramasta").jumlah(50).nama_barang("Laptop").build();
-		GetAssetResponse response2 = GetAssetResponse.builder().id(2452L).fullname("Darmawan").jumlah(40).nama_barang("Hp").build();
-		list.add(response1);
-		list.add(response2);
-		return list;
-	}
+//	@GetMapping("/users")
+//	public ArrayList<GetAssetResponse> asset(){
+//		ArrayList<GetAssetResponse> list = new ArrayList<>();
+//		GetAssetResponse response1 = GetAssetResponse.builder().id(2323L).fullname("Bramasta").jumlah(50).nama_barang("Laptop").build();
+//		GetAssetResponse response2 = GetAssetResponse.builder().id(2452L).fullname("Darmawan").jumlah(40).nama_barang("Hp").build();
+//		list.add(response1);
+//		list.add(response2);
+//		return list;
+//	}
 
+	@GetMapping("/assets/{id}")
+	public GetAssetResponse getAssetById(
+			@PathVariable Long id
+	){
+		return GetAssetResponse.builder().id(id).fullname("Bram").build();
+	}
 
 }
