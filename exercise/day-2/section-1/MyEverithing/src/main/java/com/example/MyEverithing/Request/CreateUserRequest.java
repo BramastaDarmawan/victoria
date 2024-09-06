78edeb7d2852e94cb66981f5263af35c50ade67e
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-06 18:28:01 +0700

    Spring Boot

---
+++ exercise/day-2/section-1/MyEverithing/src/main/java/com/example/MyEverithing/Request/CreateUserRequest.java
@@ -0,0 +1,23 @@
+package com.example.MyEverithing.Request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+public class CreateUserRequest {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("deskripsi")
+    private String deskripsi;
+}
---
+++ exercise/day-2/section-1/MyEverithing/src/main/java/com/example/MyEverithing/Response/UpdateUserResponse.java
@@ -0,0 +1,23 @@
+package com.example.MyEverithing.Response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+public class UpdateUserResponse {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("deskripsi")
+    private String deskripsi;
+}
