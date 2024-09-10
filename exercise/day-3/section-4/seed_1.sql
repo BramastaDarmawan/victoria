f1fdb385163362cf036a85dbf35a74aeb45d7b0c
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-10 08:21:42 +0700

    Security

--- exercise/day-3/section-4/seed_1.sql
+++ exercise/day-3/section-4/seed_1.sql
@@ -1,15 +1,18 @@
 DO $$
 DECLARE 
     i INT; 
+    dept_count INT := 5;
 BEGIN 
     FOR i IN 1..1000 LOOP
         INSERT INTO book (title, isbn, author_id, genre_id, published_date) 
         VALUES (
+            i,
             'Title_' || i,  
             'ISBN-_' || i,
             (CURRENT_DATE - (floor(random() * 365 * 5))::int)
         );
         
+        -- Insert salary for each employee
         INSERT INTO author (author_name, bio) 
         VALUES (
             i, 
